9997260dcbb17640e48c0b5059797d5e5f59fea0
alenumerek <damian.parejko@gmail.com>
2023-05-15 21:50:37 +0200

    Create trait validation rules

---
+++ app/Traits/Validations/ValidationCompanyTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits\Validates;
+
+trait ValidationCompanyTrait 
+{
+    protected function getValidationRulesCompany(){
+        return [
+            'name' => 'required|min:2',
+            'NIP' => 'required|digits:10',
+            'address' => 'required|min:2',
+            'city' => 'required|min:2',
+            'postCode' => 'required|min:5',
+            
+        ];
+    }    
+
+    protected function getValidationRulesWorker()
+    {
+        return [
+            'workers' => 'required|array|min:1',
+            'workers.*.firstName' => 'required|min:2',
+            'workers.*.lastName' => 'required|min:2',
+            'workers.*.email' => 'required|email',
+            'workers.*.phone' => 'nullable|min:8',
+        ];
+    }
+}
---
+++ app/Traits/Validations/ValidationWorkerTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits\Validates;
+
+trait ValidationWorkerTrait 
+{
+    protected function getValidationRulesCompany(){
+        return [
+            'company_id' => 'required|exists:companies,id'
+        ];
+    }
+
+    protected function getValidationRulesWorker()
+    {
+        return [
+            'firstName' => 'required|min:2',
+            'lastName' => 'required|min:2',
+            'email' => 'required|email',
+            'phone' => 'nullable|min:8',
+        ];
+    }
+}
